ff616ac04175d5549e3b563e172dc862d73811ad
contr4s <vasya617ya@mail.com>
2021-04-14 15:31:08 +0300

    add player ship controls

---
+++ Assets/Scripts/Gameplay/PlayerShip.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : Singleton<PlayerShip>
+{
+    [Header("Set in Inspector")]
+    public float shipSpeed = 10f;
+    public GameObject bulletPrefab;
+
+    Rigidbody _rigid;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float aX = Input.GetAxis("Horizontal");
+        float aY = Input.GetAxis("Vertical");
+
+        Vector3 vel = new Vector3(aX, aY);
+        if (vel.magnitude > 1)
+        {
+            // Avoid speed multiplying by sqrt(2) when moving at a diagonal
+            vel.Normalize();
+        }
+
+        _rigid.velocity = vel * shipSpeed;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //TO DO: Fire();
+        }
+    }
+
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "asteroid")
+        {
+            //TO DO: extraxt life
+        }
+    }
+
+    void Fire()
+    {
+        Vector3 mPos = Input.mousePosition;
+        mPos.z = -Camera.main.transform.position.z;
+        Vector3 mPos3D = Camera.main.ScreenToWorldPoint(mPos);
+
+        GameObject go = Instantiate<GameObject>(bulletPrefab);
+        go.transform.position = transform.position;
+        go.transform.LookAt(mPos3D);
+    }
+}
---
+++ Assets/Scripts/Gameplay/ShipPointAtMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPointAtMouse : MonoBehaviour {
+    private Vector3 _mousePoint3D;
+    private Camera _mainCam;
+
+    private void Awake() {
+        _mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        PointAtMouse();
+    }
+
+    private void PointAtMouse()
+    {
+        _mousePoint3D = _mainCam.ScreenToWorldPoint(Input.mousePosition + Vector3.back * Camera.main.transform.position.z);
+        transform.LookAt(_mousePoint3D, Vector3.back);
+    }
+}
